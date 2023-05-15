ff2822898ef7668d25d099f469f9315f8ab51d46
Syed <syed.abbas@lyftyfy.com>
2023-05-16 01:50:12 +0500

    bug fixed

--- resources/js/Pages/New/User/Form.vue
+++ resources/js/Pages/New/User/Form.vue
@@ -43,7 +43,6 @@
                                     <div>
                                         <input id="password" v-model="form.password" name="password" type="password"  class="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
                                         <span v-if="errors.password" v-text="errors.password"></span>
-                                        {{errors}}
                                     </div>
                                 </div>
 
